8a0b15e1e9a0c250f1aaebfa8cad711c27ff9868
Amey Kulkarni <kulkarniamay60@gmail.com>
2018-10-22 17:44:14 +0530

    Codes for Assignment-1

---
+++ Assignment-1/3_gcd.s
@@ -0,0 +1,17 @@
+     AREA     appcode, CODE, READONLY
+     export __main	 
+	 ENTRY 
+__main  function
+	          MOV r0 , #10	  ;value of a	
+			  MOV r1 , #6     ;value of b
+LOOP			  CMP r0 , r1
+              IT EQ 
+              BEQ STOP	
+              ITE HI			  
+			  SUBHI r0 , r0 , r1 			  
+			  SUBLS r1 , r1 , r0
+              B LOOP			  
+STOP		      B STOP  ; stop program
+        endfunc
+      end
+
